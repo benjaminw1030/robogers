59fd2dde5d0792ea94793628bb47ed1f21ffb542
Benjamin Wilson <benjaminw1030@gmail.com>
2020-03-09 22:21:47 -0700

    passed Beep spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,24 @@ var robogers = function(number) {
   var robogersArray = [];
   for (var i = 0; i <= number; i++) {
     robogersArray.push(i);
-  }
-  return robogersArray.join(", ");
-}
+  };
+  var robogersArrayMod = robogersArray.map(function(number) {
+    var digits = number.toString().split("");
+    if (digits.includes("1")) {
+      return "Beep";
+    } else {
+      return number;
+    };
+  });
+  return robogersArrayMod.join(", ");
+};
 
 // user interface logic
 $(document).ready(function() {
   $("form#mr-robogers").submit(function(event) {
     event.preventDefault();
     var number = $("input#input").val();
+    debugger
     var robogerOutput = robogers(number);
     $("#output").text(robogerOutput);
     $("#output").show();
